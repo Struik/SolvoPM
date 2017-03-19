8efaa886f1dd7ef12259e707ac859c7cbb04559e
Struik <struikol@gmail.com>
2017-03-19 18:51:49 +0300

    Reforming payments table and form

--- SolvoPM/settings.py
+++ SolvoPM/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_DIRS = (BASE_DIR, 'WebPM/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'WebPM/static')]
 STATIC_URL = '/WebPM/static/'
-#STATIC_ROOT = (BASE_DIR, '/static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGGING = {
     'version': 1,
